3298e3b2f8d2918668c7d04a6840e14ccd844e1d
MACGalaviz <macgalaviz@hotmail.com>
2019-03-20 14:51:17 -0700

    Se agregó bootstrap 4.3

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ const privateKey = "f8363d6b854ce808838b929e91003cb2556983fb",
     });
 }*/
 
-const getConnection = () => {
+/*const getConnection = () => {
     const ts = Date.now();
     const hash = md5(ts + privateKey + publicKey);
     const url = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
@@ -41,6 +41,8 @@ const drawHero = e =>{
     const image = `${e.thumbnail.path}/portrait_uncanny.${e.thumbnail.extension}`;
 
     const hero = `
+    
+    
     <div class="hero ed-item l-1-3">
         <h3>${e.name}</h3>
         <div class="hero-img">
@@ -71,5 +73,56 @@ search.addEventListener("keyup", e => {
        content.innerHTML = "";
        searchHero(e.target.value.trim());
    }
+});
+getConnection();*/
+
+const getConnection = () => {
+
+    const url = `http://localhost:3977/api/product`;
+    fetch(url).then(response => response.json()).then(response => {
+        console.log(response);
+        response.forEach(e => {
+            drawHero(e);
+        });
+    }).catch(e=>console.log(e));
+};
+
+const drawHero = e =>{
+    //const image = `${e.thumbnail.path}/portrait_uncanny.${e.thumbnail.extension}`;
+
+    const hero = `
+    
+    
+    <div class="hero ed-item l-1-3">
+        <h3>${e.name}</h3>
+        <div class="hero-img">
+            
+            <p >${e.description}</p>
+            <p >${e.price}</p>
+        </div>
+    </div>
+    `;
+    content.insertAdjacentHTML('beforeEnd',hero);
+};
+
+const searchHero = name =>{
+    const ts = Date.now();
+    const hash = md5(ts + privateKey + publicKey);
+    const hero = encodeURIComponent(name);
+    const url = `http://gateway.marvel.com/v1/public/characters?name=${hero}&ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(response => {
+            response.data.results.forEach(e => {
+                drawHero(e);
+            });
+        })
+        .catch(e=>console.log(e));
+};
+search.addEventListener("keyup", e => {
+    if(e.keyCode === 13){
+        content.innerHTML = "";
+        searchHero(e.target.value.trim());
+    }
 });
 getConnection();
