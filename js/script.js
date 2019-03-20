d2be2a1cb774a709d41279796e649cc17ab74cea
MACGalaviz <macgalaviz@hotmail.com>
2019-03-20 15:50:51 -0700

    Se agregó la funcionalidad offline mediante el uso de localStorage

--- js/script.js
+++ js/script.js
@@ -1,90 +1,28 @@
-const privateKey = "f8363d6b854ce808838b929e91003cb2556983fb",
-    publicKey = "cebe2e39110d84fa2562a234511553c7",
-    content = document.getElementById("content"),
+const content = document.getElementById("content"),
     search = document.getElementById("search");
 
-/*const getConnection = () => {
-    const ts = Date.now();
-    const hash = md5(ts + privateKey + publicKey);
-    const url = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
-    fetch(url).then(response => console.log(response));
-}*/
-
-/*const getConnection = () => {
-    const ts = Date.now();
-    const hash = md5(ts + privateKey + publicKey);
-    const url = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
-    fetch(url).then(response => {
-        if(response.status === 200)
-        {
-            console.log(response);
-        }
-        else{
-            console.log(response);
-        }
-    });
-}*/
-
-/*const getConnection = () => {
-    const ts = Date.now();
-    const hash = md5(ts + privateKey + publicKey);
-    const url = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
-    fetch(url).then(response => response.json()).then(response => {
-console.log(response);
-        response.data.results.forEach(e => {
-           drawHero(e);
-        });
-    }).catch(e=>console.log(e));
-};
-
-const drawHero = e =>{
-    const image = `${e.thumbnail.path}/portrait_uncanny.${e.thumbnail.extension}`;
-
-    const hero = `
-    
-    
-    <div class="hero ed-item l-1-3">
-        <h3>${e.name}</h3>
-        <div class="hero-img">
-            <img class="thumbnail" src="${image}">
-            <p class="description">${e.description}</p>
-        </div>
-    </div>
-    `;
-    content.insertAdjacentHTML('beforeEnd',hero);
-};
-
- const searchHero = name =>{
-     const ts = Date.now();
-     const hash = md5(ts + privateKey + publicKey);
-     const hero = encodeURIComponent(name);
-     const url = `http://gateway.marvel.com/v1/public/characters?name=${hero}&ts=${ts}&apikey=${publicKey}&hash=${hash}`;
-     fetch(url)
-         .then(response => response.json())
-         .then(response => {
-             response.data.results.forEach(e => {
-                 drawHero(e);
-             });
-         })
-         .catch(e=>console.log(e));
- };
-search.addEventListener("keyup", e => {
-   if(e.keyCode === 13){
-       content.innerHTML = "";
-       searchHero(e.target.value.trim());
-   }
-});
-getConnection();*/
 
 const getConnection = () => {
 
     const url = `http://localhost:3977/api/product`;
     fetch(url).then(response => response.json()).then(response => {
-        console.log(response);
-        response.forEach(e => {
+        localStorage.setItem("data",JSON.stringify(response))
+        console.log(localStorage.getItem("data"));
+        console.log(JSON.parse(localStorage.getItem("data")))
+
+        /*response.forEach(e => {
             drawHero(e);
-        });
-    }).catch(e=>console.log(e));
+        });*/
+    }).catch(e=>{
+        console.log(e)
+    });
+};
+
+const useData = () =>{
+    const data = JSON.parse(localStorage.getItem("data"));
+    data.forEach(e=>{
+        drawHero(e);
+    })
 };
 
 const drawHero = e =>{
@@ -96,7 +34,6 @@ const drawHero = e =>{
     <div class="hero ed-item l-1-3">
         <h3>${e.name}</h3>
         <div class="hero-img">
-            
             <p >${e.description}</p>
             <p >${e.price}</p>
         </div>
@@ -105,7 +42,7 @@ const drawHero = e =>{
     content.insertAdjacentHTML('beforeEnd',hero);
 };
 
-const searchHero = name =>{
+/*const searchHero = name =>{
     const ts = Date.now();
     const hash = md5(ts + privateKey + publicKey);
     const hero = encodeURIComponent(name);
@@ -124,5 +61,6 @@ search.addEventListener("keyup", e => {
         content.innerHTML = "";
         searchHero(e.target.value.trim());
     }
-});
+});*/
 getConnection();
+useData()
