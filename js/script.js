9e51509b96975e9bf77034720d2f4d6c32760d6d
MACGalaviz <macgalaviz@hotmail.com>
2019-03-27 10:26:54 -0700

    Se agregó la funcionalidad de buscar el host mediante un input/search

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const content = document.getElementById("content"),
-    search = document.getElementById("search");
+    host = document.getElementById("host");
 
 
-const getConnection = () => {
+const getConnection = host => {
 
-    const url = `http://localhost:3977/api/product`;
+    const url = `http://${host}:3977/api/product`;
     fetch(url).then(response => response.json()).then(response => {
         localStorage.setItem("data",JSON.stringify(response))
         console.log(localStorage.getItem("data"));
@@ -21,19 +21,19 @@ const getConnection = () => {
 const useData = () =>{
     const data = JSON.parse(localStorage.getItem("data"));
     data.forEach(e=>{
-        drawHero(e);
+        drawItem(e);
     })
 };
 
-const drawHero = e =>{
+const drawItem = e =>{
     //const image = `${e.thumbnail.path}/portrait_uncanny.${e.thumbnail.extension}`;
 
     const hero = `
     
     
-    <div class="hero ed-item l-1-3">
+    <div class="ed-item l-1-3">
         <h3>${e.name}</h3>
-        <div class="hero-img">
+        <div class="item-img">
             <p >${e.description}</p>
             <p >${e.price}</p>
         </div>
@@ -42,25 +42,11 @@ const drawHero = e =>{
     content.insertAdjacentHTML('beforeEnd',hero);
 };
 
-/*const searchHero = name =>{
-    const ts = Date.now();
-    const hash = md5(ts + privateKey + publicKey);
-    const hero = encodeURIComponent(name);
-    const url = `http://gateway.marvel.com/v1/public/characters?name=${hero}&ts=${ts}&apikey=${publicKey}&hash=${hash}`;
-    fetch(url)
-        .then(response => response.json())
-        .then(response => {
-            response.data.results.forEach(e => {
-                drawHero(e);
-            });
-        })
-        .catch(e=>console.log(e));
-};
-search.addEventListener("keyup", e => {
+host.addEventListener("keyup", e => {
     if(e.keyCode === 13){
         content.innerHTML = "";
-        searchHero(e.target.value.trim());
+        getConnection(e.target.value.trim());
     }
-});*/
-getConnection();
-useData()
+});
+
+useData();
